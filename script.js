72c5e971d6f4fea6157e3190318fbfb438dbe86b
MANITEJA KUNTA <129951439+Maniteja1122@users.noreply.github.com>
2025-03-01 11:54:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function insert_Row() {
     //Write your code here
-  
-  
+	let btn = document.getElementsByTagName("input")
+	let tableBody = document.querySelector("#sampleTable")
+	let row = tableBody.insertRow(0);
+	let cell1 = row.insertCell(0);
+	let cell2 = row.insertCell(1);
+
+	cell1.innerHTML = "New Cell1"
+	cell2.innerHTML = "New Cell2"
+	
 }
